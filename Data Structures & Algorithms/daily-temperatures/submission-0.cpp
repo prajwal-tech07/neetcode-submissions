8b46f638def89504475721c6e7c48eb11013934f
Prajwal Ragavendra Hawaldar <ugcet2300177@reva.edu.in>
2026-06-25 09:45:18 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> result;
+        
+        for(int i=0;i<temperatures.size()-1;i++){
+            bool flag=false;
+            for(int j=i+1;j<temperatures.size();j++){
+                if(temperatures[i]<temperatures[j]){
+                    result.push_back(j-i);
+                    flag=true;
+                    break;
+                }
+
+            }
+            if(!flag){
+                result.push_back(0);
+            }
+
+        }
+        result.push_back(0);
+        return result;
+    }
+};
