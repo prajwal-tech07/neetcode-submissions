e40c041e699d7d1fd87ce733631b56263daf94cf
Prajwal Ragavendra Hawaldar <ugcet2300177@reva.edu.in>
2026-07-24 09:58:26 +0530

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+      unordered_set<int> s;
+      for(int i=0;i<nums.size();i++){
+        if(s.find(nums[i])==s.end()){
+            s.insert(nums[i]);
+        }else{
+            return nums[i];
+        }
+      }
+      return -1;
+    }
+};
