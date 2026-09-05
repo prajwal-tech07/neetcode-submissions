52f696ebfc4e0a67b735ad14891bb887e43bea4c
Prajwal Ragavendra Hawaldar <ugcet2300177@reva.edu.in>
2026-09-05 12:41:42 +0530

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void dfs(int n,vector<vector<int>>&adj,vector<bool>&visited){
+      visited[n]=true;
+      for(int neighbor:adj[n]){
+        if(!visited[neighbor]){
+            dfs(neighbor,adj,visited);
+        }
+      }
+    }
+    int countComponents(int n, vector<vector<int>>& edges) {
+       int count=0;
+       vector<vector<int>> adj(n);
+       for(auto edge:edges){
+        int u=edge[0];
+        int v=edge[1];
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+       }
+       vector<bool> visited(n,false);
+       
+       for(int i=0;i<n;i++){
+           if(!visited[i]){
+            dfs(i,adj,visited);
+            count++;
+           }
+       }
+       return count;
+    }
+};
